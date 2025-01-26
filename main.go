543d945f920ce9965b37950602cb50587a21122d
kojirou <kameikojirou@gmail.com>
2025-01-25 22:55:50 -0700

    run build program or use ... LD_LIBRARY_PATH=. go run .

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 /*
 #cgo LDFLAGS: -L. -lmathlib
+#cgo LDFLAGS: -Wl,-rpath=\$ORIGIN
 #include <stdint.h>
 
 // Declare the Zig functions.
@@ -10,7 +11,9 @@ int32_t multiply(int32_t a, int32_t b);
 char* hello(char* name);
 */
 import "C"
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
     a, b := int32(3), int32(5)
@@ -18,7 +21,10 @@ func main() {
     sum := C.add(C.int32_t(a), C.int32_t(b))
     product := C.multiply(C.int32_t(a), C.int32_t(b))
     hello := C.hello(C.CString("Ziggy"))
+
     fmt.Printf("Sum: %d\n", int32(sum))
     fmt.Printf("Product: %d\n", int32(product))
-    fmt.Printf("Hello: %s\n",  C.GoString(hello))
+    fmt.Printf("Hello: %s\n", C.GoString(hello))
+    hello = C.hello(C.CString("ZiggyZoop"))
+    fmt.Printf("Hello: %s\n", C.GoString(hello))
 }
